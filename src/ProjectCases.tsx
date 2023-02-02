0db6416659ea142b0c8e431d2770584af0eb8777
Tomáš Sabol <tomassabol2@gmail.com>
2023-02-02 14:58:08 +0100

    optimization and projectcase route

---
+++ src/ProjectCases.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+
+const ProjectCases: React.FC = () => {
+
+    useEffect(() => {
+        window.location.replace('https://tomassabol.notion.site/project-cases-we-like-32b3425090c7434982fd02ded25bd7e4');
+    }, []);
+
+    return <></>
+};
+
+export default ProjectCases;
