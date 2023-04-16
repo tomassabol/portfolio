1d170ec2a694095f9b93b10fbf934d18732758c7
Tomáš Sabol <tomassabol2@gmail.com>
2023-04-16 07:42:34 +0200

    rewritten in astro

--- src/custom.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.png';
-declare module '*.module.css';
-declare module '*.svg';
-declare module '*.webp';
-
-declare module '*.svg' {
-  const content: any;
-  export default content;
-}
